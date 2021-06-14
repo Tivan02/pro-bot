b9591d76367f5893e458f2c712acd1ee43e63017
Tivan02 <84316809+Tivan02@users.noreply.github.com>
2021-06-14 21:18:37 +0200

    Update register.php

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
 		//inserite vostro indirizzo su heroku 
 		//https://username.herokuapp.com/execute.php
 		//inserisci l'ure dell'execute su heroku es: https://telegram2.herokuapp.com/execute.php
-		$WEBHOOK_URL = 'inserisci l'url dell tuo file execute su heroku';
+		$WEBHOOK_URL = 'https://pro-bot12345.herokuapp.com/execute.php';
 		//inserite il vostro token che vi ha fornito l'utente telegram @botfather
 		$BOT_TOKEN = 'Inserisci_Token_Telegram';
 
